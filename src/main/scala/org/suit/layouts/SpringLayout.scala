4f1a3a7cbc75e6de23560a85fe864a0ec84fe1cb
stevendobay <istvandobai7@gmail.com>
2015-05-12 22:32:20 +0200

    Initializing SpringLayout.

---
+++ src/main/scala/org/suit/layouts/SpringLayout.scala
@@ -0,0 +1,64 @@
+/**
+ * Copyright© Steven Dobay 2015
+ */
+package org.suit.layouts
+
+import org.suit._
+
+/**
+ * @author Steven Dobay
+ * @version 0.1
+ * Wrapper for java's spring-layout.
+ */
+case class SpringLayout() extends Layout {
+  private val layout = new javax.swing.SpringLayout()
+
+  /**
+   * @param constraint1 : SpringLayout._
+   * @param comp1
+   * @param value
+   * @param constraint2 : SpringLayout._
+   * @param comp2
+   */
+  def addConstraint(constraint1: String, comp1: Component,
+                    value: Int, constraint2: String, comp2: Component): Unit =
+    layout.putConstraint(constraint1, comp1.wrapped, value,
+                         constraint2, comp2.wrapped)
+
+  /**
+   * @param constraint : SpringLayout._
+   * @param comp
+   * @return
+   */
+  def getConstraint(constraint: String, comp: Component) =
+    layout.getConstraint(constraint, comp.wrapped)
+
+  /**
+   * @param comp
+   */
+  def remove(comp: Component) = layout.removeLayoutComponent(comp.wrapped)
+
+  /**
+   * @param comp
+   * @return
+   */
+  def constraints(comp: Component) = layout.getConstraints(comp.wrapped)
+
+  /**
+   * @return with the wrapped layout manager.
+   */
+  protected[suit] def wrapped = layout
+}
+
+object SpringLayout {
+  import javax.swing.{SpringLayout => SL}
+
+  val NORTH = SL.NORTH
+  val SOUTH = SL.SOUTH
+  val WEST = SL.WEST
+  val EAST = SL.EAST
+  val BASELINE = SL.BASELINE
+  val WIDTH = SL.WIDTH
+  val HEIGHT = SL.HEIGHT
+  val HORIZONTAL_CENTER = SL.HORIZONTAL_CENTER
+}
